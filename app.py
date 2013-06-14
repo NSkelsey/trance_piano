f589e59535d43b97b9bd3a9c12c2967290742cef
NickS <nskelsey@gmail.com>
2013-06-14 12:10:20 -0400

    more better proccessing

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, abort
 import json
+from urlparse import urlparse
 
 app = Flask(__name__)
 
-with open('site.json', 'r') as f:
+with open('./mikeSite.txt', 'r') as f:
     raw = f.read()
     url_dict = json.loads(raw)
 
-def normalize_url(path):
+def normalize_path(path):
     if path == '':
         path = '/'
     if not path.startswith('/'):
@@ -16,13 +17,44 @@ def normalize_url(path):
         path = path + '/'
     return path
 
-_copy_d = dict()
-for key, value in url_dict.iteritems():
-    key = normalize_url(key)
-    _copy_d[key] = [normalize_url(v) for v in value]
-url_dict = _copy_d
+def build_from_scrape(url_dict):
+    _copy_d = dict()
+    for key, value in url_dict.iteritems():
+        key = normalize_path(key)
+        _copy_d[key] = [normalize_path(v) for v in value]
+    url_dict = _copy_d
+    return url_dict
 
-print "These paths will all generate a valid response:\n", url_dict.keys()
+# returns tuple of (if internal link, path) <-- (false, none) if no
+def format_internal(url, parsed_site):
+    url = urlparse(url)
+    if url.scheme != 'http':
+        return (False, None)
+    if (url.netloc == parsed_site.netloc 
+      or url.netloc == '.'.join(parsed_site.netloc.split('.')[-2:])):
+        return (True, url.path)
+    else: return (False, None)
+
+def build_from_referrers(url_dict, root_url):
+    parsed = urlparse(root_url)
+    _copy_d = dict()
+    for key in url_dict.keys():
+        if key not in _copy_d: 
+            key_path = normalize_path(key)
+            _copy_d[key_path] = set()
+    for key, values in url_dict.iteritems():
+        key = normalize_path(key)
+        for refer in values:
+            internal, path = format_internal(refer, parsed)
+            if internal:
+                path = normalize_path(path)
+                if path in _copy_d:
+                    _copy_d[key].add(path)
+    return _copy_d
+
+url_dict = build_from_referrers(url_dict, "http://www.plagiarismtoday.com")
+
+print url_dict
 
 @app.route('/', defaults={'path':''})
 @app.route('/<path:path>')
